b57dba0b1748089f24b3ee4bc830dc1a3426b977
Ben Daniels <benbanj@hotmail.com>
2019-03-27 17:48:14 +0000

    Completing main logic

--- script1.js
+++ script1.js
@@ -1,31 +1,47 @@
-let start = document.querySelector('.start-btn');
-let main = document.querySelector('main');
-start.addEventListener('click', function(e){
-  start.style.display = "none";
-  main.style.display = "flex";
-});
 
 let playerScore = 0;
 let compScore = 0;
+let start = document.querySelector('.start-btn');
+let main = document.querySelector('main');
+let scorePanel = document.querySelector('#scores');
+let endBtn = document.querySelector('.end-btn');
+let container = document.querySelector('#text-content');
 
 document.querySelector('#player-score').innerHTML = playerScore;
 document.querySelector('#comp-score').innerHTML = compScore;
 
+playGame();
 
-function game() {
-  console.log(playerScore);
+function playGame() {
+  start.addEventListener('click', function(e){
+    start.style.display = "none";
+    main.style.display = "flex";
+    scorePanel.style.display = "flex";
+  });
+  selectChoice();
 }
 
-/*Records choice then plays round*/
-window.addEventListener('click', function(e){
-  removeContent();
-  let choice = e.srcElement.name;
-  if (!choice) {
-    return
-  } else {
-    playRound(choice, computerPlay())
-  }
-})
+function endGame() {
+  main.style.display = "none";
+  endBtn.style.display = "block";
+  container.style.display = "none";
+  endBtn.addEventListener('click', function() {
+    window.location.reload(false); 
+  });
+}
+
+function selectChoice() {
+  /*Records choice then plays round*/
+  window.addEventListener('click', function(e){
+    removeContent();
+    let choice = e.srcElement.name;
+    if (!choice) {
+      return
+    } else {
+      playRound(choice, computerPlay())
+    }
+  })
+}
 
 function computerPlay() {
   let arr = ["rock", "paper", "scissors"];
@@ -38,7 +54,6 @@ function capitalize(str) {
 }
 
 function displayContent(text) {
-  let container = document.querySelector('#text-content');
   let content = document.createElement('p');
   content.textContent = text;
   container.appendChild(content);
@@ -57,7 +72,7 @@ function playRound(plySelect, comSelect) {
     displayContent(`Computer chose ${capitalize(comSelect)}`);
     setTimeout(function() {
       displayContent("It's a Draw!");
-    }, 2000);
+    }, 1000);
   } else if (plySelect === "rock" && comSelect === "paper" ||
              plySelect === "paper" && comSelect === "scissors" ||
              plySelect === "scissors" && comSelect === "rock") {
@@ -65,21 +80,31 @@ function playRound(plySelect, comSelect) {
     displayContent(`Computer chose ${capitalize(comSelect)}`);
     setTimeout(function() {
       displayContent(`You lose! ${capitalize(comSelect)} beats ${capitalize(plySelect)}`);
-    }, 2000);
+    }, 1000);
     compScore += 1;
     setTimeout(function() {
       document.querySelector('#comp-score').innerHTML = compScore;
-    }, 3000);
+    }, 2000);
+    if (compScore == 5) {
+      setTimeout(function() {
+        endGame();
+      }, 3000);
+    }
   } else {
     displayContent(`You chose ${capitalize(plySelect)}`);
     displayContent(`Computer chose ${capitalize(comSelect)}`);
     setTimeout(function() {
       displayContent(`You win! ${capitalize(plySelect)} beats ${capitalize(comSelect)}`);
-    }, 2000);
+    }, 1000);
     playerScore += 1;
     setTimeout(function() {
       document.querySelector('#player-score').innerHTML = playerScore;
-    }, 3000);
+    }, 2000);
+    if (playerScore == 5) {
+      setTimeout(function() {
+        endGame();
+      }, 3000);
+    }
   }
 }
 
