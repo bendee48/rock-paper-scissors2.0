0657fa68ccff7ecc10531a582e9cebc4197a858b
Ben Daniels <benbanj@hotmail.com>
2019-03-27 12:42:01 +0000

    Adding scoring and text updates

--- script1.js
+++ script1.js
@@ -5,8 +5,20 @@ start.addEventListener('click', function(e){
   main.style.display = "flex";
 });
 
+let playerScore = 0;
+let compScore = 0;
+
+document.querySelector('#player-score').innerHTML = playerScore;
+document.querySelector('#comp-score').innerHTML = compScore;
+
+
+function game() {
+  console.log(playerScore);
+}
+
 /*Records choice then plays round*/
 window.addEventListener('click', function(e){
+  removeContent();
   let choice = e.srcElement.name;
   if (!choice) {
     return
@@ -25,76 +37,100 @@ function capitalize(str) {
   return res;
 }
 
-function test() {
-  let arr = ["hello", "bye", "hey"];
-  for (i in arr) {
-    let container = document.querySelector('#text-content');
-    let content = document.createElement("div");
-    content.textContent = i;
-    container.appendChild(content);
+function displayContent(text) {
+  let container = document.querySelector('#text-content');
+  let content = document.createElement('p');
+  content.textContent = text;
+  container.appendChild(content);
+}
+
+function removeContent() {
+  let container = document.querySelector('#text-content');
+  while (container.lastChild) {
+    container.removeChild(container.lastChild);
   }
 }
 
 function playRound(plySelect, comSelect) {
-  /*plySelect = plySelect.toLowerCase();*/
   if (plySelect === comSelect) {
-    console.log("It's a Draw!");
-    return 0;
+    displayContent(`You chose ${capitalize(plySelect)}`);
+    displayContent(`Computer chose ${capitalize(comSelect)}`);
+    setTimeout(function() {
+      displayContent("It's a Draw!");
+    }, 2000);
   } else if (plySelect === "rock" && comSelect === "paper" ||
              plySelect === "paper" && comSelect === "scissors" ||
-             plySelect == "scissors" && comSelect === "rock") {
-    console.log(`You lose! ${capitalize(comSelect)} beats ${capitalize(plySelect)}`);
-    return -1;
+             plySelect === "scissors" && comSelect === "rock") {
+    displayContent(`You chose ${capitalize(plySelect)}`);
+    displayContent(`Computer chose ${capitalize(comSelect)}`);
+    setTimeout(function() {
+      displayContent(`You lose! ${capitalize(comSelect)} beats ${capitalize(plySelect)}`);
+    }, 2000);
+    compScore += 1;
+    setTimeout(function() {
+      document.querySelector('#comp-score').innerHTML = compScore;
+    }, 3000);
   } else {
-    console.log(`You win ${capitalize(plySelect)} beats ${capitalize(comSelect)}`);
-    return 1;
+    displayContent(`You chose ${capitalize(plySelect)}`);
+    displayContent(`Computer chose ${capitalize(comSelect)}`);
+    setTimeout(function() {
+      displayContent(`You win! ${capitalize(plySelect)} beats ${capitalize(comSelect)}`);
+    }, 2000);
+    playerScore += 1;
+    setTimeout(function() {
+      document.querySelector('#player-score').innerHTML = playerScore;
+    }, 3000);
   }
-
 }
 
-function answerBad(answer) {
-  if (answer === "rock" || answer === "paper" || answer === "scissors" ) {
-    return false;
-  } else {
-    return true;
-  }
-}
 
-function game() {
-  let playerScore = 0;
-  let compScore = 0;
 
-  for (let i = 0; i < 5; i++) {
-    let answer = prompt("Rock, Paper or Scissors?");
-    answer = answer.toLowerCase();
 
-    while (answerBad(answer)) {
-      secAnswer = prompt("Incorrect value: You need to enter either 'rock', 'paper' or 'scissors'.");
-      answer = secAnswer.toLowerCase();
-    }
 
-    res = playRound(answer, computerPlay());
-    switch (res) {
-      case 1:
-        playerScore++;
-        break;
-      case -1:
-        compScore++;
-        break;
-    }
-    console.log(res);
-    console.log(`Current scores: Player = ${playerScore}, Computer = ${compScore}`);
-  }
 
-  if (playerScore > compScore) {
-    console.log("Congratulations, you win!");
-    console.log(`Final score: Player = ${playerScore}, Computer = ${compScore}`);
-  } else if (compScore > playerScore) {
-    console.log("Ah boo, you lose!");
-    console.log(`Final score: Player = ${playerScore}, Computer = ${compScore}`);
-  } else {
-    console.log("After all that it was a draw.");
-    console.log(`Final score: Player = ${playerScore}, Computer = ${compScore}`);
-  }
+// function answerBad(answer) {
+//   if (answer === "rock" || answer === "paper" || answer === "scissors" ) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
 
-}
+// function game() {
+//   let playerScore = 0;
+//   let compScore = 0;
+//
+//   for (let i = 0; i < 5; i++) {
+//     let answer = prompt("Rock, Paper or Scissors?");
+//     answer = answer.toLowerCase();
+//
+//     while (answerBad(answer)) {
+//       secAnswer = prompt("Incorrect value: You need to enter either 'rock', 'paper' or 'scissors'.");
+//       answer = secAnswer.toLowerCase();
+//     }
+//
+//     res = playRound(answer, computerPlay());
+//     switch (res) {
+//       case 1:
+//         playerScore++;
+//         break;
+//       case -1:
+//         compScore++;
+//         break;
+//     }
+//     console.log(res);
+//     console.log(`Current scores: Player = ${playerScore}, Computer = ${compScore}`);
+//   }
+//
+//   if (playerScore > compScore) {
+//     console.log("Congratulations, you win!");
+//     console.log(`Final score: Player = ${playerScore}, Computer = ${compScore}`);
+//   } else if (compScore > playerScore) {
+//     console.log("Ah boo, you lose!");
+//     console.log(`Final score: Player = ${playerScore}, Computer = ${compScore}`);
+//   } else {
+//     console.log("After all that it was a draw.");
+//     console.log(`Final score: Player = ${playerScore}, Computer = ${compScore}`);
+//   }
+//
+// }
